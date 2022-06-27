416804c688fcca1f47d8a2f16088fe7c87d19cc1
Jackie <2537020964@qq.com>
2022-06-27 15:31:00 +0800

    init

---
+++ packages/components/JList/src/JListItemContent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="j-list-item-title">
+    <span class="j-list-item-title__text">
+      <slot></slot>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "JListItemTitle",
+};
+</script>
+
+<style lang="scss" scoped>
+@include b(list-item-text) {
+  display: flex;
+  align-items: center;
+  font-size: 20px;
+  font-weight: 600;
+  @include e(text) {
+    // transform: translateY(-1px);
+  }
+}
+</style>
